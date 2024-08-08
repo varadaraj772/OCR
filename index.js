e8331e392f3a6dee4b07217a0ad0a24e2c7d557c
varadaraj772 <varadaraj277@gmail.com>
2024-08-08 17:52:54 +0530

    changes

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 /**
  * @format
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-AppRegistry.registerComponent(appName, () => App);
+const Main = () => (
+  <SafeAreaProvider>
+    <App />
+  </SafeAreaProvider>
+);
+
+AppRegistry.registerComponent(appName, () => Main);
